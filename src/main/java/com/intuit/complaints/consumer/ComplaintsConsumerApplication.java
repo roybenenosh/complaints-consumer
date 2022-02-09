4e1ff75dc5fbac46110f55de8e97f9cfbc0dc481
roybenenosh <roybenenosh@gmail.com>
2022-02-09 11:52:30 +0200

    Finish implementation

--- src/main/java/com/intuit/complaints/consumer/ComplaintsConsumerApplication.java
+++ src/main/java/com/intuit/complaints/consumer/ComplaintsConsumerApplication.java
@@ -2,8 +2,10 @@ package com.intuit.complaints.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.intuit.complaints.dal")
 public class ComplaintsConsumerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/intuit/complaints/consumer/core/ComplaintsConsumer.java
+++ src/main/java/com/intuit/complaints/consumer/core/ComplaintsConsumer.java
@@ -4,21 +4,16 @@ import com.intuit.complaints.consumer.ComplaintRepository;
 import com.intuit.complaints.dal.Complaint;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class ComplaintsConsumer {
 
     private final ComplaintRepository complaintRepository;
-
-    @Lazy
-    public ComplaintsConsumer(ComplaintRepository complaintRepository) {
-        this.complaintRepository = complaintRepository;
-    }
 
     @KafkaListener(topics = "complaints", groupId = "group_id")
     public void consume(Complaint complaint) {
